2955523c24c8d2f146f482a51548cdaf92fb7559
Vitali_Kozh <vitalik24021992@gmail.com>
2026-01-28 15:55:34 +0300

    feat: add tests for comments

--- __tests__/e2e/comments/comments.test.ts
+++ __tests__/e2e/comments/comments.test.ts
@@ -1,14 +1,13 @@
 import { ObjectId } from 'mongodb';
 
+import { jwtService } from '../../../src/auth/application';
 import { CommentViewModelType } from '../../../src/comments/types';
 import { APP_ROUTES } from '../../../src/core/constants';
 import { HTTP_STATUSES, ResponseWithPaginationType } from '../../../src/core/types';
 import { mockComment } from '../../utils/comments/mock';
 import { createPost } from '../../utils/posts/create-post';
 import { TestManager } from '../../utils/test-manager';
-import { createUser } from '../../utils/users/create-user';
 import { loginUser } from '../../utils/users/login-user';
-import { mockUser } from '../../utils/users/mock';
 
 type CommentsResponseType = ResponseWithPaginationType<CommentViewModelType>;
 
@@ -47,8 +46,6 @@ describe('Comments test', () => {
         .set('Authorization', `Bearer ${accessToken}`)
         .send(mockComment);
 
-      console.log(createdComment.body);
-
       await testManager.context
         .request()
         .get(`${APP_ROUTES.COMMENTS}/${createdComment.body.id}`)
@@ -58,33 +55,202 @@ describe('Comments test', () => {
     });
   });
 
-  describe.skip('POST /posts/:id/comments', () => {
+  describe('PUT /comments/:id', () => {
+    it('should return 401 status code if incorrect access token', async () => {
+      const fakePostId = new ObjectId().toString();
+
+      await testManager.context
+        .request()
+        .put(`${APP_ROUTES.COMMENTS}/${fakePostId}`)
+        .set('Authorization', `Bearer ${'incorrect token'}`)
+        .send(mockComment)
+        .expect(HTTP_STATUSES.UNAUTHORIZED);
+    });
+
+    it('should return 400 status code if incorrect payload data', async () => {
+      const createdPost = await createPost(testManager);
+      const accessToken = await loginUser(testManager);
+
+      const createdComment = await testManager.context
+        .request()
+        .post(`${APP_ROUTES.POSTS}/${createdPost.id}${APP_ROUTES.COMMENTS}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(mockComment);
+
+      await testManager.context
+        .request()
+        .put(`${APP_ROUTES.COMMENTS}/${createdComment.body.id}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send({ content: 'small content' })
+        .expect(HTTP_STATUSES.BAD_REQUEST);
+    });
+
+    it('should return 403 status code if do not have permissions', async () => {
+      const createdPost = await createPost(testManager);
+      const accessToken = await loginUser(testManager);
+
+      const createdComment = await testManager.context
+        .request()
+        .post(`${APP_ROUTES.POSTS}/${createdPost.id}${APP_ROUTES.COMMENTS}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(mockComment);
+
+      const token = jwtService.createJWT({ userId: 'random' });
+
+      await testManager.context
+        .request()
+        .put(`${APP_ROUTES.COMMENTS}/${createdComment.body.id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(mockComment)
+        .expect(HTTP_STATUSES.FORBIDDEN);
+    });
+
+    it('should return 404 status code if incorrect comment id', async () => {
+      const fakeCommentId = new ObjectId().toString();
+      const accessToken = await loginUser(testManager);
+
+      await testManager.context
+        .request()
+        .put(`${APP_ROUTES.COMMENTS}/${fakeCommentId}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(mockComment)
+        .expect(HTTP_STATUSES.NOT_FOUND);
+    });
+  });
+
+  describe('DELETE /comments/:id', () => {
     it('should return 401 status code if token invalid', async () => {
-      const createdUser = await createUser(testManager);
+      const fakeCommentId = new ObjectId().toString();
+
+      await testManager.context
+        .request()
+        .delete(`${APP_ROUTES.COMMENTS}/${fakeCommentId}`)
+        .set('Authorization', `Bearer ${'invalid_token'}`)
+        .expect(HTTP_STATUSES.UNAUTHORIZED);
+    });
+
+    it('should return 403 status code if do not have permissions', async () => {
+      const createdPost = await createPost(testManager);
+      const accessToken = await loginUser(testManager);
+
+      const createdComment = await testManager.context
+        .request()
+        .post(`${APP_ROUTES.POSTS}/${createdPost.id}${APP_ROUTES.COMMENTS}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(mockComment);
+
+      const incorrectToken = jwtService.createJWT({ userId: 'incorrect user' });
+
+      await testManager.context
+        .request()
+        .delete(`${APP_ROUTES.COMMENTS}/${createdComment.body.id}`)
+        .set('Authorization', `Bearer ${incorrectToken}`)
+        .expect(HTTP_STATUSES.FORBIDDEN);
+    });
+
+    it('should return 404 status code if incorrect comment id', async () => {
+      const fakeCommentId = new ObjectId().toString();
+      const accessToken = await loginUser(testManager);
+
+      await testManager.context
+        .request()
+        .delete(`${APP_ROUTES.COMMENTS}/${fakeCommentId}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(HTTP_STATUSES.NOT_FOUND);
+    });
+
+    it('should return 204 status code', async () => {
       const createdPost = await createPost(testManager);
 
-      const { body } = await testManager.context
+      const accessToken = await loginUser(testManager);
+
+      const createdComment = await testManager.context
         .request()
-        .post(`${APP_ROUTES.AUTH}${APP_ROUTES.AUTH_LOGIN}`)
-        .send({ loginOrEmail: createdUser.login, password: mockUser.password });
+        .post(`${APP_ROUTES.POSTS}/${createdPost.id}${APP_ROUTES.COMMENTS}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(mockComment);
+
+      await testManager.context
+        .request()
+        .delete(`${APP_ROUTES.COMMENTS}/${createdComment.body.id}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(HTTP_STATUSES.NO_CONTENT);
+    });
+  });
+
+  describe('POST /posts/:id/comments', () => {
+    it('should return 401 status code if token invalid', async () => {
+      const createdPost = await createPost(testManager);
 
       await testManager.context
         .request()
         .post(`${APP_ROUTES.POSTS}/${createdPost.id}${APP_ROUTES.COMMENTS}`)
-        .set('Authorization', `Bearer ${body.accessToken}`)
-        .send({ content: 'dlkfkj' })
+        .set('Authorization', `Bearer ${'fake-token'}`)
+        .expect(HTTP_STATUSES.UNAUTHORIZED);
+    });
+
+    it('should return 400 status code if invalid payload data', async () => {
+      const createdPost = await createPost(testManager);
+      const accessToken = await loginUser(testManager);
+
+      await testManager.context
+        .request()
+        .post(`${APP_ROUTES.POSTS}/${createdPost.id}${APP_ROUTES.COMMENTS}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(HTTP_STATUSES.BAD_REQUEST);
+    });
+
+    it('should return 404 status code if post not exist', async () => {
+      const fakePostId = new ObjectId().toString();
+      const accessToken = await loginUser(testManager);
+
+      await testManager.context
+        .request()
+        .post(`${APP_ROUTES.POSTS}/${fakePostId}${APP_ROUTES.COMMENTS}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(mockComment)
+        .expect(HTTP_STATUSES.NOT_FOUND);
+    });
+
+    it('should return 201 status code and created comment', async () => {
+      const createdPost = await createPost(testManager);
+      const accessToken = await loginUser(testManager);
+
+      await testManager.context
+        .request()
+        .post(`${APP_ROUTES.POSTS}/${createdPost.id}${APP_ROUTES.COMMENTS}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(mockComment)
         .expect(HTTP_STATUSES.CREATED);
     });
   });
+
+  describe('GET /posts/:id/comments', () => {
+    it('should return 404 status code if incorrect post id', async () => {
+      const fakePostId = new ObjectId().toString();
+
+      await testManager.context
+        .request()
+        .get(`${APP_ROUTES.POSTS}/${fakePostId}${APP_ROUTES.COMMENTS}`)
+        .expect(HTTP_STATUSES.NOT_FOUND);
+    });
+
+    it('should return 200 status code with comments', async () => {
+      const createdPost = await createPost(testManager);
+      const accessToken = await loginUser(testManager);
+
+      const commentData = await testManager.context
+        .request()
+        .post(`${APP_ROUTES.POSTS}/${createdPost.id}${APP_ROUTES.COMMENTS}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(mockComment);
+
+      const comments = await testManager.context
+        .request()
+        .get(`${APP_ROUTES.POSTS}/${createdPost.id}${APP_ROUTES.COMMENTS}`)
+        .expect(HTTP_STATUSES.OK);
+
+      expect(comments.body.items).toEqual([commentData.body]);
+    });
+  });
 });
-
-// it('should return 401 status code if access token invalid', async () => {
-//       const createdPost = await createPost(testManager);
-
-// await testManager.context
-//   .request()
-//   .post(`${APP_ROUTES.POSTS}/${createdPost.id}${APP_ROUTES.COMMENTS}`)
-//   .set('Authorization', `Bearer invalid_token`)
-//   .send(mockComment)
-//   .expect(HTTP_STATUSES.UNAUTHORIZED);
-//     });
