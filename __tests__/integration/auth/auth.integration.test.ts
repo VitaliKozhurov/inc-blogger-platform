d5bd00071c98228753b2e441b1a1a538b34512ce
Vitali_Kozh <vitalik24021992@gmail.com>
2026-02-06 09:09:37 +0300

    refactor: rewrite integration tests

--- __tests__/integration/auth/auth.integration.test.ts
+++ __tests__/integration/auth/auth.integration.test.ts
@@ -1,3 +1,4 @@
+import { add } from 'date-fns';
 import { Db, ObjectId } from 'mongodb';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 
@@ -6,29 +7,34 @@ import { authService } from '../../../src/auth/application';
 import { HTTP_STATUSES } from '../../../src/core/types';
 import { RESULT_STATUSES } from '../../../src/core/utils';
 import { runDB, stopDb } from '../../../src/db/mongo.db';
+import { UserDBType } from '../../../src/users/types';
 
 import { SETTINGS } from './../../../src/core/settings/settings';
 
 describe('Auth test', () => {
   // const testManager = new TestManager();
-  // const confirmationCode = '123';
+  const confirmationCode = '123';
   let DB: Db;
 
-  // const createUser = (emailConfirmation: Partial<UserDBType['emailConfirmation']> = {}) => {
-  //   return {
-  //     _id: new ObjectId(),
-  //     createdAt: '',
-  //     email: SETTINGS.APP_EMAIL_ADDRESS ?? '',
-  //     login: '',
-  //     passwordHash: '',
-  //     emailConfirmation: {
-  //       confirmationCode,
-  //       expirationDate: add(new Date(), { hours: 1 }).toISOString(),
-  //       isConfirmed: false,
-  //       ...emailConfirmation,
-  //     },
-  //   };
-  // };
+  const createUser = async (emailConfirmation: Partial<UserDBType['emailConfirmation']> = {}) => {
+    const user = {
+      _id: new ObjectId(),
+      createdAt: new Date().toISOString(),
+      email: SETTINGS.APP_EMAIL_ADDRESS ?? '',
+      login: 'super_user',
+      passwordHash: '',
+      emailConfirmation: {
+        confirmationCode,
+        expirationDate: add(new Date(), { hours: 1 }).toISOString(),
+        isConfirmed: true,
+        ...emailConfirmation,
+      },
+    };
+
+    await DB.collection('users').insertOne(user);
+
+    return user;
+  };
 
   beforeAll(async () => {
     const mongoServer = await MongoMemoryServer.create();
@@ -59,19 +65,8 @@ describe('Auth test', () => {
       expect(emailRegistrationAdapter.sendConfirmationCode).toHaveBeenCalled();
     });
 
-    it('should return a 400 status code if send incorrect data', async () => {
-      await DB.collection('users').insertOne({
-        _id: new ObjectId(),
-        createdAt: new Date().toISOString(),
-        email: SETTINGS.APP_EMAIL_ADDRESS ?? '',
-        login: 'super_user',
-        passwordHash: '',
-        emailConfirmation: {
-          confirmationCode: '',
-          expirationDate: '',
-          isConfirmed: true,
-        },
-      });
+    it('should return a 400 status code if send credentials with the same user in system', async () => {
+      await createUser();
 
       const result = await authService.registration({
         login: 'super_user',
@@ -79,58 +74,80 @@ describe('Auth test', () => {
         password: 'secret_password',
       });
 
-      console.log(result);
-
       expect(result.status).toBe(HTTP_STATUSES.BAD_REQUEST);
       expect(emailRegistrationAdapter.sendConfirmationCode).not.toHaveBeenCalled();
     });
   });
 
-  // describe.skip('POST /auth/registration-confirmation', () => {
-  //   it('should return a 204 status code if send correct code', async () => {
-  //     await usersCollection.insertOne(createUser());
+  describe('POST /auth/registration-confirmation', () => {
+    it('should return a success status code if email success verified', async () => {
+      const createdUser = await createUser({ isConfirmed: false });
 
-  //     const result = await authService.registrationConfirmation(confirmationCode);
+      const result = await authService.registrationConfirmation(
+        createdUser.emailConfirmation.confirmationCode
+      );
 
-  //     expect(result.status).toBe(HTTP_STATUSES.OK);
-  //   });
+      expect(result.status).toBe(RESULT_STATUSES.OK);
+      const user = await DB.collection('users').findOne({ _id: createdUser._id });
 
-  //   it('should return a 400 status code if send incorrect code', async () => {
-  //     await usersCollection.insertOne(createUser({ confirmationCode: '456' }));
+      expect(user?.emailConfirmation.isConfirmed).toBe(true);
+    });
 
-  //     const result = await authService.registrationConfirmation(confirmationCode);
+    it('should return status 400 if user has confirm status', async () => {
+      const createdUser = await createUser();
 
-  //     expect(result.status).toBe(HTTP_STATUSES.BAD_REQUEST);
-  //   });
+      const result = await authService.registrationConfirmation(
+        createdUser.emailConfirmation.confirmationCode
+      );
 
-  //   it('should return a 400 status code if send expired code', async () => {
-  //     await usersCollection.insertOne(createUser({ expirationDate: new Date().toISOString() }));
+      expect(result.status).toBe(RESULT_STATUSES.BAD_REQUEST);
+    });
 
-  //     const result = await authService.registrationConfirmation(confirmationCode);
+    it('should return status 400 if send incorrect confirmation code', async () => {
+      const result = await authService.registrationConfirmation('invalid_code');
 
-  //     expect(result.status).toBe(HTTP_STATUSES.BAD_REQUEST);
-  //   });
-  // });
+      expect(result.status).toBe(RESULT_STATUSES.BAD_REQUEST);
+    });
 
-  // describe.skip('POST /auth/registration-email-resending', () => {
-  //   it('should return a 204 status code if send correct code', async () => {
-  //     await usersCollection.insertOne(createUser());
+    // it('should return a 400 status code if send credentials with the same user in system', async () => {
+    //   await createUser();
 
-  //     const result = await authService.registrationEmailResending({
-  //       email: SETTINGS.APP_EMAIL_ADDRESS ?? '',
-  //     });
+    //   const result = await authService.registration({
+    //     login: 'super_user',
+    //     email: SETTINGS.APP_EMAIL_ADDRESS ?? '',
+    //     password: 'secret_password',
+    //   });
 
-  //     expect(result.status).toBe(HTTP_STATUSES.OK);
-  //   });
+    //   expect(result.status).toBe(HTTP_STATUSES.BAD_REQUEST);
+    //   expect(emailRegistrationAdapter.sendConfirmationCode).not.toHaveBeenCalled();
+    // });
+  });
 
-  //   it('should return a 400 status code if user email status is confirmed', async () => {
-  //     await usersCollection.insertOne(createUser({ isConfirmed: true }));
+  describe('POST /auth/registration-email-confirmation', () => {
+    jest.spyOn(emailRegistrationAdapter, 'resendConfirmationCode').mockResolvedValue(true);
 
-  //     const result = await authService.registrationEmailResending({
-  //       email: SETTINGS.APP_EMAIL_ADDRESS ?? '',
-  //     });
+    it('should return a 204 status code if send correct email for resending confirmation code', async () => {
+      const createdUser = await createUser({ isConfirmed: false });
 
-  //     expect(result.status).toBe(HTTP_STATUSES.BAD_REQUEST);
-  //   });
-  // });
+      const result = await authService.registrationEmailResending({ email: createdUser.email });
+
+      expect(result.status).toBe(HTTP_STATUSES.OK);
+      expect(emailRegistrationAdapter.resendConfirmationCode).toHaveBeenCalled();
+    });
+
+    it('should return a 400 status code if send not correct email', async () => {
+      const result = await authService.registrationEmailResending({ email: 'fake@email.com' });
+
+      expect(result.status).toBe(HTTP_STATUSES.BAD_REQUEST);
+      expect(emailRegistrationAdapter.resendConfirmationCode).not.toHaveBeenCalled();
+    });
+
+    it('should return a 400 status code if account is confirmed', async () => {
+      const createdUser = await createUser();
+      const result = await authService.registrationEmailResending({ email: createdUser.email });
+
+      expect(result.status).toBe(HTTP_STATUSES.BAD_REQUEST);
+      expect(emailRegistrationAdapter.resendConfirmationCode).not.toHaveBeenCalled();
+    });
+  });
 });
