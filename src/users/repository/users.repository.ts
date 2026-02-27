ab2b0890c05765b6179aa7443c8a065a383e54dc
VitaliKozhurov <vitalik24021992@gmail.com>
2026-02-27 22:15:18 +0300

    refactor: change user repository

--- src/users/repository/users.repository.ts
+++ src/users/repository/users.repository.ts
@@ -1,62 +1,40 @@
 import { injectable } from 'inversify';
-import { ObjectId, WithId } from 'mongodb';
+import { ObjectId } from 'mongodb';
 
 import { Nullable } from '../../core/types';
-import { UserDBType } from '../types';
+import { UserDocument, UserModel, UserType } from '../model';
 
 @injectable()
 export class UsersRepository {
-  async createUser(userData: UserDBType) {
-    const { insertedId } = await usersCollection.insertOne(userData);
+  async createUser(userData: Omit<UserType, '_id'>) {
+    const { id } = await UserModel.create(userData);
 
-    return insertedId.toString();
+    return id;
   }
 
   async deleteUserById(id: string) {
-    const { deletedCount } = await usersCollection.deleteOne({ _id: new ObjectId(id) });
+    const { deletedCount } = await UserModel.deleteOne({ _id: new ObjectId(id) });
 
     return deletedCount > 0;
   }
 
-  async updateUserById({ id, userData }: { id: string; userData: UserDBType }) {
-    const { modifiedCount } = await usersCollection.updateOne(
-      { _id: new ObjectId(id) },
-      { $set: userData }
-    );
-
-    return modifiedCount > 0;
+  async saveUser(user: UserDocument) {
+    await user.save();
   }
 
-  async updateUserPasswordByUserId({ id, passwordHash }: { id: string; passwordHash: string }) {
-    const { modifiedCount } = await usersCollection.updateOne(
-      { _id: new ObjectId(id) },
-      { $set: { passwordHash }, $unset: { passwordRecovery: '' } }
-    );
-
-    return modifiedCount > 0;
+  async getUserById(id: string): Promise<Nullable<UserDocument>> {
+    return UserModel.findById(id).exec();
   }
 
-  async getUserByLoginOrEmail(loginOrEmail: string): Promise<WithId<UserDBType> | null> {
-    const user = await usersCollection.findOne({
-      $or: [{ login: loginOrEmail }, { email: loginOrEmail }],
-    });
-
-    return user;
+  async getUserByLoginOrEmail(loginOrEmail: string): Promise<Nullable<UserDocument>> {
+    return UserModel.findOne({ $or: [{ login: loginOrEmail }, { email: loginOrEmail }] }).exec();
   }
 
-  async getUserByConfirmationCode(code: string): Promise<WithId<UserDBType> | null> {
-    const user = await usersCollection.findOne({ 'emailConfirmation.confirmationCode': code });
-
-    return user;
+  async getUserByConfirmationCode(code: string): Promise<Nullable<UserDocument>> {
+    return UserModel.findOne({ 'emailConfirmation.confirmationCode': code });
   }
 
-  async getUserByRecoveryCode(code: string): Promise<WithId<UserDBType> | null> {
-    const user = await usersCollection.findOne({ 'passwordRecovery.recoveryCode': code });
-
-    return user;
-  }
-
-  async getUserById(id: string): Promise<Nullable<WithId<UserDBType>>> {
-    return usersCollection.findOne({ _id: new ObjectId(id) });
+  async getUserByRecoveryCode(code: string): Promise<Nullable<UserDocument>> {
+    return UserModel.findOne({ 'passwordRecovery.recoveryCode': code });
   }
 }
